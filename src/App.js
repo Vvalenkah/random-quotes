d5b6e9e84751f0ae0c882ca74d46222ba12c1931
Vvalenkah <vano-sai@tut.by>
2022-08-19 21:29:01 +0300

    init: start project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios  from 'axios';
+import './style.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+state = {
+joke: '',
+// price: '0.3'
 }
 
+  GetJoke = async () => {
+    const {data} = await axios.get(`http://www.boredapi.com/api/activity/`);
+    // ?price=${this.state.price}
+    this.setState({joke: data.activity})
+    return
+  }
+
+  
+  componentDidMount() {
+    this.GetJoke()
+}
+
+  showMe() {
+    console.log(this.state.joke)
+  }
+
+  render() {
+    // this.GetJoke()
+    const {joke} = this.state;
+    console.log(joke)
+    return (
+      <div className="main-container">
+      <p>{joke}</p>
+      <button className="change-btn" onClick={this.GetJoke}>Show Activity</button>
+      </div>
+    )
+  }
+}
 export default App;
