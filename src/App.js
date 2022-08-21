58e2a4dfb5b59cbae4ae6c9caa7a9f92eb3ddcb9
Vvalenkah <vano-sai@tut.by>
2022-08-21 18:55:24 +0300

    feat: add adaptive and styles

--- src/App.js
+++ src/App.js
@@ -21,18 +21,24 @@ joke: '',
     this.GetJoke()
 }
 
-  showMe() {
-    console.log(this.state.joke)
-  }
-
   render() {
     // this.GetJoke()
     const {joke} = this.state;
     console.log(joke)
     return (
-      <div className="main-container">
-      <p>{joke}</p>
-      <button className="change-btn" onClick={this.GetJoke}>Show Activity</button>
+      <div className="body-wrapper">
+        <header>
+          <h1>Activity Advisor</h1>
+          <p>By Vvalenkah</p>
+        </header>
+        <div className="main-container">
+        <p>{joke}</p>
+        <button className="change-btn" onClick={this.GetJoke}>What to do?</button>
+        </div>
+        <footer>
+          <a href="https://github.com/Vvalenkah" target="blank">My Github</a>
+          <p>2022</p>
+        </footer>
       </div>
     )
   }
